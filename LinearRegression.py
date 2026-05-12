a28e190648df783e0097e58662fb1bdf0b887afe
Vitor Souza <105333945+VitzLok@users.noreply.github.com>
2026-05-12 20:27:47 -0300

    Add files via upload

---
+++ LinearRegression.py
@@ -0,0 +1,36 @@
+from numpy import *
+ 
+class LinearRegression:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.__correlation_coefficient = self.__correlacao()
+        self.__inclination = self.__inclinacao()
+        self.__intercept = self.__interceptacao()
+
+    def __correlacao(self):
+        covariacao = cov(self.x, self.y, bias=True)[0][1]
+        variancia_x = var(self.x)
+        variancia_y = var(self.y)
+        return covariacao / sqrt(variancia_x * variancia_y)
+      
+    def __inclinacao(self):
+        stdx = std(self.x)
+        stdy = std(self.y)
+        return self.__correlation_coefficient * (stdy / stdx)
+      
+    def __interceptacao(self):
+        mediax = mean(self.x)
+        mediay = mean(self.y)
+        return mediay - mediax * self.__inclination
+      
+    def previsao(self, valor):      
+        return self.__intercept + (self.__inclination * valor)
+    
+x = array([1,2,3,4,5])
+y = array([2,4,6,8,10])    
+
+lr = LinearRegression(y,x)
+previsao = lr.previsao(int(input("Digite um valor para que seja feita a previsão:")))
+print(f"A previsão foi feita e o número que equivale a Y na matriz de X é: {previsao}")
+                                                                            
